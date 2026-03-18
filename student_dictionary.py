e715dd723affaa17dfaf49ad1d56ef7bc6701f15
Asi-Web <agblezea@students.trinitydc.edu>
2026-03-17 21:08:12 -0400

    Add student dictionary program
    Create student dictionary with name, age, and major and print it

---
+++ student_dictionary.py
@@ -0,0 +1,14 @@
+## Asiwome Agbleze
+## Trinity Washington University
+## CMSC 111/1 - Michael Agee
+## March 17, 2026
+
+# Creating a dictionary to store a student's information
+student = {
+    "name": "John Doe",
+    "age": 20,
+    "major": "Computer Science"
+}
+
+# Print the dictionary
+print(student)
